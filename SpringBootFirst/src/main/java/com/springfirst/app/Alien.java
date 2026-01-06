02391e368b60c2fd60f7e6d7b1dacd240ddd0d7c
rolandparragh <torcsiii05@gmail.com>
2026-01-06 17:00:46 +0100

    last from laptop

--- SpringBootFirst/src/main/java/com/springfirst/app/Alien.java
+++ SpringBootFirst/src/main/java/com/springfirst/app/Alien.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class Alien {
 	
 	@Autowired
 	Laptop laptop; 
+	// The Alien class is a Spring component. The @Autowired annotation tells Spring to automatically
+	// inject a Laptop bean into the laptop field when creating an Alien object. This is called dependency injection.
+	// When the code() method is called, it uses the injected Laptop to call its compile() method.
+
 	
 	public void code() {
 		
